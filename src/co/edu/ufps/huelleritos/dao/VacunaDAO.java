fc6afa85a4fd690fa423dc9683a9dd561f8c1676
Diego Calderon <est_dm_calderon@fesc.edu.co>
2021-05-19 19:52:15 -0500

    subiendo otra vez

--- src/co/edu/ufps/huelleritos/dao/VacunaDAO.java
+++ src/co/edu/ufps/huelleritos/dao/VacunaDAO.java
@@ -1,6 +1,6 @@
 package co.edu.ufps.huelleritos.dao;
 
-import co.edu.ufps.huelleritos.entities.Usuario;
+
 import co.edu.ufps.huelleritos.entities.Vacuna;
 import co.edu.ufps.huelleritos.util.Conexion;
 import co.edu.ufps.huelleritos.util.GenericDAO;
